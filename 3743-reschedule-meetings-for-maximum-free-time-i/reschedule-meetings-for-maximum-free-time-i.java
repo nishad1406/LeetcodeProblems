8888f1b106a8958c1a57bba14c1077d6fbcad552
nishad1406 <144772934+nishad1406@users.noreply.github.com>
2025-07-10 07:35:13 +0530

    Time: 1 ms (100.00%) | Memory: 63.1 MB (54.19%) - LeetSync

--- 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.java
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.java
@@ -1,30 +1,20 @@
-class Solution {
+public class Solution {
     public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
-        int n = startTime.length;
+        int count = startTime.length;
+        int max = 0;
+        int[] prefixSum = new int[count + 1];
 
-        List<Integer> gaps = new ArrayList<>();
-
-        // calculating gaps
-        gaps.add(startTime[0]);
-        for (int i = 1; i < n; i++) {
-            gaps.add(startTime[i] - endTime[i - 1]);
-        }
-        gaps.add(eventTime - endTime[n - 1]);
-
-        int currentSum = 0, res = 0;
-
-        // use sliding window on gaps array to find maximum possible gap
-        // for K meetings there K+1 gaps, thus window size = K+1
-
-        for (int i = 0; i < gaps.size(); ++i) {
-
-            // form window of size k+1
-            // if i >= k+1, add new element and remove i-(k+1) element
-            currentSum += gaps.get(i) - ((i >= k + 1) ? gaps.get(i - (k + 1)) : 0);
-            res = Math.max(res, currentSum);
+        for (int i = 0; i < count; i++) {
+            prefixSum[i + 1] = prefixSum[i] + endTime[i] - startTime[i];
         }
 
-        return res;
+        for (int i = k - 1; i < count; i++) {
+            int occupied = prefixSum[i + 1] - prefixSum[i - k + 1];
+            int windowEnd = (i == count - 1) ? eventTime : startTime[i + 1];
+            int windowStart = (i == k - 1) ? 0 : endTime[i - k];
+            max = Math.max(max, windowEnd - windowStart - occupied);
+        }
 
+        return max;
     }
 }
