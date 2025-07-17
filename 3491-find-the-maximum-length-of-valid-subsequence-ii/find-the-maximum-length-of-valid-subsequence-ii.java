7663e159a1b97f2054e20d298cd1bc8d4af37358
nishad1406 <144772934+nishad1406@users.noreply.github.com>
2025-07-17 11:24:15 +0530

    Time: 31 ms (96.91%) | Memory: 54.4 MB (27.84%) - LeetSync

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.java
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.java
@@ -1,69 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int maximumLength(int[] nums, int k) {
-        int n = nums.length;
-        int[] r = new int[n];
-        for (int i = 0; i < n; i++) {
-            r[i] = nums[i] % k;
-        }
-        
-        List<List<Integer>> l = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
-            l.add(new ArrayList<>());
-        }
-        for (int i = 0; i < n; i++) {
-            l.get(r[i]).add(i);
-        }
-        
-        int m = 0;
-        for (List<Integer> lst : l) {
-            m = Math.max(m, lst.size());
-        }
-        
-        if (k == 0 || k == 1) {
-            return m;
-        }
-        
-        for (int x = 0; x < k; x++) {
-            for (int y = x + 1; y < k; y++) {
-                List<Integer> a = l.get(x);
-                List<Integer> b = l.get(y);
-                int la = a.size();
-                int lb = b.size();
-                if (la == 0 && lb == 0) {
-                    continue;
-                }
-                int i = 0, j = 0;
-                int sx = 0, sy = 0;
-                while (i < la || j < lb) {
-                    if (j == lb || (i < la && a.get(i) < b.get(j))) {
-                        int nx = 1;
-                        if (sy > 0) {
-                            nx = sy + 1;
-                        }
-                        if (nx > sx) {
-                            sx = nx;
-                        }
-                        i++;
-                    } else {
-                        int ny = 1;
-                        if (sx > 0) {
-                            ny = sx + 1;
-                        }
-                        if (ny > sy) {
-                            sy = ny;
-                        }
-                        j++;
-                    }
-                }
-                int c = Math.max(sx, sy);
-                if (c > m) {
-                    m = c;
-                }
+        int[][] dp = new int[k][k];
+        int res = 0;
+        for(int num:nums){
+            num %= k;
+            for(int prev=0;prev<k;prev++){
+                dp[prev][num] = dp[num][prev] + 1;
+                res = Math.max(res,dp[prev][num]);
             }
         }
-        return m;
+        return res;
     }
 }
