42f9232b634ab3c7855648e2ef0de926374883c2
nishad1406 <144772934+nishad1406@users.noreply.github.com>
2025-07-29 07:58:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (7.78%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int setBitCount = 0;
+		while (n != 0) {
+            n &= (n - 1);
+            ++setBitCount;
+        }
+        return setBitCount;
+    }
+}
